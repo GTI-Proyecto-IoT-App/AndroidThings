2eaf3eb604d4fb8f3f46aba5e332dc92724e03de
ALBERTO <albertitovalls97@gmail.com>
2020-11-18 17:27:46 +0100

    mqtt

--- app/src/main/java/com/example/raspberry_m5stack/MainActivity.java
+++ app/src/main/java/com/example/raspberry_m5stack/MainActivity.java
@@ -9,7 +9,17 @@ import android.util.Log;
 import com.example.raspberry_m5stack.firebase.callback.CallBack;
 import com.example.raspberry_m5stack.model.MedidaBasura;
 import com.example.raspberry_m5stack.repository.impl.MedidaBasuraImpl;
+import com.google.android.things.pio.PeripheralManager;
+import com.google.gson.JsonArray;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -32,32 +42,53 @@ import java.io.IOException;
  * <p>
  * You can find additional examples on GitHub: https://github.com/androidthings
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MqttCallback {
 
+    public static MqttClient client = null;
     private static final String TAG = MainActivity.class.getSimpleName();
-    private ArduinoUart uart;
+
     private MedidaBasuraImpl medidaBasuraRepository;
 
 
     private static final String DELIMITADOR_TIPO_DISPOSITIVO = "%";
-    private static final String DELIMITADOR_MENSAJE_UART = "\\$\\$\\$\\$";
     private static final String DISPOSITIVO_BASURA = "basura";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        Log.i("TESTING", "Lista de UART disponibles: " + ArduinoUart.disponibles());
-        uart = new ArduinoUart("UART0", 115200);
+
         medidaBasuraRepository = new MedidaBasuraImpl();
         //Log.d(TAG, "Mandado a Arduino: D");
         //uart.escribir("D");
+        try {
+            Log.i(Mqtt.TAG, "Conectando al broker " + Mqtt.broker);
+            client = new MqttClient(Mqtt.broker, Mqtt.clientId,
+                    new MemoryPersistence());
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            connOpts.setKeepAliveInterval(60);
+            connOpts.setWill(Mqtt.topicRoot+"WillTopic", "App desconectada".getBytes(),Mqtt.qos, false);
+            client.connect(connOpts);
 
+        } catch (MqttException e) {
+            Log.e(Mqtt.TAG, "Error al conectar.", e);
+        }
 
         try {
+            Log.i(Mqtt.TAG, "Suscrito a " + Mqtt.topicRoot+"#");
+            client.subscribe(Mqtt.topicRoot+"#", Mqtt.qos);
+            client.setCallback(this);
+        } catch (MqttException e) {
+            Log.e(Mqtt.TAG, "Error al suscribir.", e);
+        }
+
+    }
+
+        /*try {
             int i =0;
             while(true){
-                String s = uart.leer();
+                ;
                 String[] res = s.split(DELIMITADOR_MENSAJE_UART);
                 if (res.length>1){
                     Log.d(TAG, "Recibido de Arduino -> "+i+": "+res[1]);
@@ -92,25 +123,69 @@ public class MainActivity extends AppCompatActivity {
      * @param jsonObject el json object que recibimos del m5 stack
      * @throws JSONException mal formateo del json
      */
+
+
     private void addMesuraBasura(JSONObject jsonObject) throws JSONException {
-        MedidaBasura medidasB = new MedidaBasura(jsonObject);
+
         String id = jsonObject.getString("id");
+        JSONArray medidas = jsonObject.getJSONArray("medidas");
+        for (int i = 0; i < medidas.length(); i++) {
 
-        medidaBasuraRepository.crearMedidaBasura(id,medidasB, new CallBack() {
-            @Override
-            public void onSuccess(Object object) {
-                Log.d(TAG,object.toString());
-            }
+            MedidaBasura medidasB = new MedidaBasura(medidas.getJSONObject(i));
+            medidaBasuraRepository.crearMedidaBasura(id,medidasB, new CallBack() {
+                @Override
+                public void onSuccess(Object object) {
+                    Log.d(TAG,object.toString());
+                }
 
-            @Override
-            public void onError(Object object) {
-                Log.e(TAG,object.toString());
-            }
-        });
+                @Override
+                public void onError(Object object) {
+                    Log.e(TAG,object.toString());
+                }
+            });
+        }
+
     }
 
     @Override protected void onDestroy() {
+        try {
+            Log.i(Mqtt.TAG, "Desconectado");
+            client.disconnect();
+        } catch (MqttException e) {
+            Log.e(Mqtt.TAG, "Error al desconectar.", e);
+        }
         super.onDestroy();
     }
+
+
+    @Override
+    public void connectionLost(Throwable cause) {
+        Log.d(Mqtt.TAG, "Conexión perdida");
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+        try {
+            JSONObject jsonObject = new JSONObject(message.toString());
+            String tipoDispositivo = jsonObject.getString("id").split(DELIMITADOR_TIPO_DISPOSITIVO)[1];
+
+            switch (tipoDispositivo){
+                case DISPOSITIVO_BASURA:
+                    addMesuraBasura(jsonObject);
+                    break;
+            }
+
+        }catch (JSONException err){
+            Log.d("Error", err.toString());
+        }
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken token) {
+        Log.d(Mqtt.TAG, "Entrega completa");
+    }
+
 }
+
+
 
--- app/src/main/java/com/example/raspberry_m5stack/model/MedidaBasura.java
+++ app/src/main/java/com/example/raspberry_m5stack/model/MedidaBasura.java
@@ -6,27 +6,61 @@ import org.json.JSONObject;
 public class MedidaBasura {
     //Id del dispositivo
     private String tipoMedida;
+
+
+
     //private String time;
-    private double valor;
+    private double llenado;
+    private double peso;
+
+    private long unixTime;
 
     public MedidaBasura(JSONObject obj) throws JSONException {
-        this.tipoMedida = obj.getString("tipoMedida");
-        this.valor = obj.getDouble("valor");
+        this.tipoMedida = obj.getString("tipo");
+        this.llenado = obj.getDouble("volumen");
+        //this.llenado = obj.getDouble("peso");
+        this.unixTime = System.currentTimeMillis();
     }
 
-    public MedidaBasura(String tipoMedida, double valor) {
+    public MedidaBasura(String tipoMedida, double llenado, double peso) {
         this.tipoMedida = tipoMedida;
-        this.valor = valor;
+        this.llenado = llenado;
+        this.peso = peso;
+        this.unixTime = System.currentTimeMillis();
     }
 
+
+
+    public long getUnixTime() {
+        return unixTime;
+    }
+
+    public void setUnixTime(long unixTime) {
+        this.unixTime = unixTime;
+    }
 
     public String getTipoMedida() {
         return tipoMedida;
     }
 
-
-    public double getValor() {
-        return valor;
+    public void setTipoMedida(String tipoMedida) {
+        this.tipoMedida = tipoMedida;
     }
+
+    public double getLlenado() {
+        return llenado;
+    }
+
+    public void setLlenado(double llenado) {
+        this.llenado = llenado;
+    }
+
+//    public double getPeso() {
+//        return peso;
+//    }
+//
+//    public void setPeso(double peso) {
+//        this.peso = peso;
+//    }
 
 }
